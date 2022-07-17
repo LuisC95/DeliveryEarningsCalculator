92e2b42c5ae7d684b4a8409bc52727c1a0948157
LuisC95 <luiscarmona.95@gmail.com>
2022-07-17 11:15:50 -0400

    variables and form added

---
+++ main.js
@@ -0,0 +1,10 @@
+var taxes = 1.15;
+var gasPrice = 4.40;
+var gasPerformance = 24;
+var gasByMile = gasPrice / gasPerformance; 
+var deliveryDistance; 
+var traveledDistance; 
+var deliveryOffer;
+
+
+
