fe626c9b77266472a52f5abc3af765bdbc5293ec
LuisC95 <luiscarmona.95@gmail.com>
2022-07-19 19:59:38 -0400

    operating variables

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ var taxes = 1.15;
 var gasPrice = 4.40;
 var gasPerformance = 24;
 var gasByMile = gasPrice / gasPerformance; 
-var deliveryDistance; 
-var traveledDistance; 
-var deliveryOffer;
+var earnings;
+
+var deliveryDistance = document.getElementById("distance"); 
+var traveledDistance = document.getElementById("traveled"); 
+var deliveryOffer = document.getElementById("offer");
 
 
-
+function operation(e)
+{
+    earnings = (taxes*(gasPerformance*(traveledDistance + deliveryDistance)))-deliveryOffer;
+    console.log(earnings);
+}
