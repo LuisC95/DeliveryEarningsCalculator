5e2035905a1ff92c67a760c4ade3ce5b805d776e
LuisC95 <luiscarmona.95@gmail.com>
2022-07-20 01:18:24 -0400

    first trying to get usability in the software, console in chrome does'nt work

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ var earnings;
 var deliveryDistance = document.getElementById("distance"); 
 var traveledDistance = document.getElementById("traveled"); 
 var deliveryOffer = document.getElementById("offer");
+var earnings = document.getElementById("update");
+
+earnings.addEventListener("click", updateValue);
 
 
 function operation(e)
 {
+    deliveryDistance = parseInt(e.target.value);
+    traveledDistance = parseInt(e.target.value);
+    deliveryOffer = parseInt(e.target.value);
     earnings = (taxes*(gasPerformance*(traveledDistance + deliveryDistance)))-deliveryOffer;
     console.log(earnings);
+}
+
+function updateValue(e)
+{
+    operation; 
+    alert(deliveryDistance);
 }
